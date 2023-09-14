e91f92ccdd1df5fa5966926c2e711bd2b266ae5b
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-09-14 14:57:20 +0200

    Implemented a default class for Collection

--- src/main/java/net/filipvanlaenen/kolektoj/Collection.java
+++ src/main/java/net/filipvanlaenen/kolektoj/Collection.java
@@ -1,11 +1,54 @@
 package net.filipvanlaenen.kolektoj;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Interface defining the signature for all collections.
  *
  * @param <E> The element type.
  */
 public interface Collection<E> {
+    /**
+     * Default implementation of the collection interface.
+     *
+     * @param <E> The element type.
+     */
+    final class DefaultCollection<E> implements Collection<E> {
+        /**
+         * A set with the elements.
+         */
+        private final Set<E> elements = new HashSet<E>();
+
+        @Override
+        public boolean contains(final E element) {
+            return elements.contains(element);
+        }
+
+        @Override
+        public E get() throws IndexOutOfBoundsException {
+            if (isEmpty()) {
+                throw new IndexOutOfBoundsException("Cannot return an element from an empty collection.");
+            } else {
+                return elements.iterator().next();
+            }
+        }
+
+        @Override
+        public int size() {
+            return elements.size();
+        }
+    }
+
+    /**
+     * Returns a new empty collection.
+     *
+     * @return A new empty collection.
+     */
+    static Collection empty() {
+        return new DefaultCollection();
+    }
+
     /**
      * Returns whether the collection contains the element.
      *
@@ -15,11 +58,12 @@ public interface Collection<E> {
     boolean contains(E element);
 
     /**
-     * Returns an element from the collection.
+     * Returns an element from the collection. Throws an exception if the collection is empty.
      *
      * @return An element from the collection.
+     * @throws IndexOutOfBoundsException Thrown if the collection is empty.
      */
-    E get();
+    E get() throws IndexOutOfBoundsException;
 
     /**
      * Returns whether the collection is empty or not.
