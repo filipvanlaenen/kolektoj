2a1ef57a3818a91a3b92eec720182dc9ceb062b7
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-09-15 15:21:09 +0200

    Added unit tests to ArrayCollectionTest

--- src/test/java/net/filipvanlaenen/kolektoj/array/ArrayCollectionTest.java
+++ src/test/java/net/filipvanlaenen/kolektoj/array/ArrayCollectionTest.java
@@ -1,6 +1,9 @@
 package net.filipvanlaenen.kolektoj.array;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +26,43 @@ public class ArrayCollectionTest {
     @Test
     public void sizeShouldReturnThreeForACollectionOfThreeElements() {
         assertEquals(THREE, COLLECTION123.size());
+    }
+
+    /**
+     * Verifies that contains returns true for an element in the collection.
+     */
+    @Test
+    public void containsShouldReturnTrueForAnElementInTheCollection() {
+        assertTrue(COLLECTION123.contains(1));
+    }
+
+    /**
+     * Verifies that contains returns false for an element not in the collection.
+     */
+    @Test
+    public void containsShouldReturnFalseForAnElementNotInTheCollection() {
+        assertFalse(COLLECTION123.contains(0));
+    }
+
+    /**
+     * Verifies that when you get an element from a collection, the collection contains it.
+     */
+    @Test
+    public void getShouldReturnAnElementPresentInTheCollection() {
+        Integer element = COLLECTION123.get();
+        assertTrue(COLLECTION123.contains(element));
+    }
+
+    /**
+     * Verifies that trying to get an element from an empty collection throws IndexOutOfBoundsException.
+     */
+    @Test
+    public void getShouldThrowExceptionWhenCalledOnAnEmptyCollection() {
+        try {
+            new ArrayCollection<Integer>().get();
+            fail();
+        } catch (IndexOutOfBoundsException e) {
+            assertEquals("Cannot return an element from an empty collection.", e.getMessage());
+        }
     }
 }
