11a269bfb7ca28e68ee53d5a44923230b60148f6
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-09-15 15:25:28 +0200

    Added factory methods to OrderedCollection

--- src/main/java/net/filipvanlaenen/kolektoj/OrderedCollection.java
+++ src/main/java/net/filipvanlaenen/kolektoj/OrderedCollection.java
@@ -1,11 +1,34 @@
 package net.filipvanlaenen.kolektoj;
 
+import net.filipvanlaenen.kolektoj.array.OrderedArrayCollection;
+
 /**
  * Interface defining the signature for all ordered collections.
  *
  * @param <E> The element type.
  */
 public interface OrderedCollection<E> extends Collection<E> {
+    /**
+     * Returns a new empty ordered collection.
+     *
+     * @param <E> The element type.
+     * @return A new empty ordered collection.
+     */
+    static <E> OrderedCollection<E> empty() {
+        return new OrderedArrayCollection<E>();
+    }
+
+    /**
+     * Returns a new ordered collection with the specified elements.
+     *
+     * @param <E>      The element type.
+     * @param elements The elements for the new ordered collection.
+     * @return A new ordered collection with the specified elements.
+     */
+    static <E> OrderedCollection<E> of(final E... elements) {
+        return new OrderedArrayCollection<E>(elements);
+    }
+
     /**
      * Returns the element from the collection at the given position.
      *
