026eb41b9de841ba618ca8da84c28bdbd3d63c99
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-09-20 12:55:17 +0200

    Added unit tests on OrderedArrayCollection

---
+++ src/test/java/net/filipvanlaenen/kolektoj/array/OrderedArrayCollectionTest.java
@@ -0,0 +1,77 @@
+package net.filipvanlaenen.kolektoj.array;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests on the {@link net.filipvanlaenen.kolektoj.array.OrderedArrayCollection} class.
+ */
+public class OrderedArrayCollectionTest {
+    /**
+     * The magic number three.
+     */
+    private static final int THREE = 3;
+    /**
+     * The magic number six.
+     */
+    private static final int SIX = 6;
+    /**
+     * Ordered array collection with the integers 1, 2 and 3.
+     */
+    private static final OrderedArrayCollection<Integer> COLLECTION123 = new OrderedArrayCollection<Integer>(1, 2, 3);
+
+    /**
+     * Verifies that the correct length is returned for a collection with three elements.
+     */
+    @Test
+    public void sizeShouldReturnThreeForACollectionOfThreeElements() {
+        assertEquals(THREE, COLLECTION123.size());
+    }
+
+    /**
+     * Verifies that contains returns true for an element in the collection.
+     */
+    @Test
+    public void containsShouldReturnTrueForAnElementInTheCollection() {
+        assertTrue(COLLECTION123.contains(1));
+    }
+
+    /**
+     * Verifies that contains returns false for an element not in the collection.
+     */
+    @Test
+    public void containsShouldReturnFalseForAnElementNotInTheCollection() {
+        assertFalse(COLLECTION123.contains(0));
+    }
+
+    /**
+     * Verifies that when you get an element from a collection, the collection contains it.
+     */
+    @Test
+    public void getShouldReturnAnElementPresentInTheCollection() {
+        Integer element = COLLECTION123.get();
+        assertTrue(COLLECTION123.contains(element));
+    }
+
+    /**
+     * Verifies that trying to get an element from an empty collection throws IndexOutOfBoundsException.
+     */
+    @Test
+    public void getShouldThrowExceptionWhenCalledOnAnEmptyCollection() {
+        assertThrows(IndexOutOfBoundsException.class, () -> new ArrayCollection<Integer>().get(),
+                "Cannot return an element from an empty collection.");
+    }
+
+    /**
+     * Verifies that the collection produces an array with the elements in the same order.
+     */
+    @Test
+    public void toArrayShouldProduceAnArrayWithTheElementsOfTheCollection() {
+        assertArrayEquals(new Integer[] {1, 2, 3}, COLLECTION123.toArray());
+    }
+}
