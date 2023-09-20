51630824c4b40b0c3011dae6538277dc5c496ff4
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-09-20 09:06:27 +0200

    Added unit tests on ModifiableCollection

--- src/test/java/net/filipvanlaenen/kolektoj/CollectionTest.java
+++ src/test/java/net/filipvanlaenen/kolektoj/CollectionTest.java
@@ -1,4 +1,4 @@
-package net.filipvanlaenen.kolektoj.array;
+package net.filipvanlaenen.kolektoj;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
