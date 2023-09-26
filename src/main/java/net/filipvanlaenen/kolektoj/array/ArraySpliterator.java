f0bf7113df88ebe1189ce8741ca4f6e87b9d3616
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-09-26 10:47:10 +0200

    Added unit tests to the ArraySpliterator

--- src/main/java/net/filipvanlaenen/kolektoj/array/ArraySpliterator.java
+++ src/main/java/net/filipvanlaenen/kolektoj/array/ArraySpliterator.java
@@ -4,22 +4,63 @@ import java.lang.reflect.Array;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
+/**
+ * An implementation of {@link java.util.Spliterator} that is backed by an array. This implementation provides a
+ * spliterator that is both sized and subsized, but depending on the content and the order of the elements, it may have
+ * additional characteristics.
+ *
+ * @param <E> The element type.
+ */
 final class ArraySpliterator<E> implements Spliterator<E> {
+    /**
+     * The characteristics of the spliterator.
+     */
     private final int characteristics;
+    /**
+     * The elements of the spliterator.
+     */
     private final E[] elements;
+    /**
+     * The spliterator's current position.
+     */
     private int index;
 
+    /**
+     * Constructor taking the elements and additional characteristics as its arguments.
+     *
+     * @param elements                  The elements for the spliterator.
+     * @param additionalCharacteristics The characteristics for the spliterator in addition to SIZED and SUBSIZED.
+     */
     ArraySpliterator(final E[] elements, final int additionalCharacteristics) {
         this.elements = elements.clone();
         this.characteristics = SIZED | SUBSIZED | additionalCharacteristics;
     }
 
+    /**
+     * Constructor taking the elements and additional characteristics as its arguments, in addition to the starting
+     * (inclusive) and ending (exclusive) position in the element array.
+     *
+     * @param elements        The elements for the spliterator.
+     * @param from            The position of the first element to be included.
+     * @param to              The position just after the last element to be included.
+     * @param characteristics The characteristics for the spliterator in addition to SIZED and SUBSIZED.
+     */
     private ArraySpliterator(final E[] elements, final int from, final int to, final int characteristics) {
         int size = to - from;
         Class<E[]> clazz = (Class<E[]>) elements.getClass();
         this.elements = (E[]) Array.newInstance(clazz.getComponentType(), size);
         System.arraycopy(elements, from, this.elements, 0, size);
         this.characteristics = characteristics;
+    }
+
+    @Override
+    public int characteristics() {
+        return characteristics;
+    }
+
+    @Override
+    public long estimateSize() {
+        return (long) (elements.length - index);
     }
 
     @Override
@@ -42,15 +83,5 @@ final class ArraySpliterator<E> implements Spliterator<E> {
         } else {
             return null;
         }
-    }
-
-    @Override
-    public long estimateSize() {
-        return (long) (elements.length - index);
-    }
-
-    @Override
-    public int characteristics() {
-        return characteristics;
     }
 }
---
+++ src/test/java/net/filipvanlaenen/kolektoj/array/ArraySpliteratorTest.java
@@ -0,0 +1,171 @@
+package net.filipvanlaenen.kolektoj.array;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Spliterator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests on the {@link net.filipvanlaenen.kolektoj.array.ArraySpliterator} class.
+ */
+public class ArraySpliteratorTest {
+    /**
+     * The magic number three.
+     */
+    private static final int THREE = 3;
+    /**
+     * The magic number four.
+     */
+    private static final int FOUR = 4;
+    /**
+     * The magic number five.
+     */
+    private static final int FIVE = 5;
+    /**
+     * The magic number six.
+     */
+    private static final int SIX = 6;
+    /**
+     * An array containing the numbers from 1 to 6.
+     */
+    private static final Integer[] ARRAY123456 = new Integer[] {1, 2, THREE, FOUR, FIVE, SIX};
+
+    /**
+     * Creates a new spliterator.
+     *
+     * @return A new spliterator.
+     */
+    private Spliterator<Integer> createNewSpliterator() {
+        return new ArraySpliterator<Integer>(ARRAY123456, 0);
+    }
+
+    /**
+     * Verifies that the size of a new spliterator is correct.
+     */
+    @Test
+    public void estimateSizeShouldReturnCorrectSize() {
+        assertEquals(SIX, createNewSpliterator().estimateSize());
+    }
+
+    /**
+     * Verifies that tryAdvance returns true for a new spliterator.
+     */
+    @Test
+    public void tryAdvanceShouldReturnTrueForANewSpliterator() {
+        assertTrue(createNewSpliterator().tryAdvance(Integer::longValue));
+    }
+
+    /**
+     * Verifies that calling tryAdvance makes the length of the spliterator shorter.
+     */
+    @Test
+    public void tryAdvanceShouldReduceTheLengthByOne() {
+        Spliterator<Integer> spliterator = createNewSpliterator();
+        spliterator.tryAdvance(Integer::longValue);
+        assertEquals(FIVE, spliterator.estimateSize());
+    }
+
+    /**
+     * Verifies that tryAdvance calls the consumer.
+     */
+    @Test
+    public void tryAdanceShouldCallTheConsumer() {
+        StringBuffer sb = new StringBuffer();
+        createNewSpliterator().tryAdvance((x) -> sb.append(x));
+        assertEquals("1", sb.toString());
+    }
+
+    /**
+     * Verifies that tryAdvance returns false after having been called as many times as there are elements.
+     */
+    @Test
+    public void tryAdvanceShouldReturnFalseAfterTheLastElement() {
+        Spliterator<Integer> spliterator = createNewSpliterator();
+        for (int i = 0; i < SIX; i++) {
+            spliterator.tryAdvance(Integer::longValue);
+        }
+        assertFalse(spliterator.tryAdvance(Integer::longValue));
+    }
+
+    /**
+     * Verifies that trySplit splits the spliterator in two equal-sized spliterators.
+     */
+    @Test
+    public void trySplitShouldSplitTheSpliteratorInTwoEqualSizedSpliterators() {
+        Spliterator<Integer> spliterator = createNewSpliterator();
+        Spliterator<Integer> spliterator2 = spliterator.trySplit();
+        assertEquals(THREE, spliterator.estimateSize());
+        assertEquals(THREE, spliterator2.estimateSize());
+    }
+
+    /**
+     * Verifies that trySplit splits the spliterator in two almost equal-sized spliterators after advancing once.
+     */
+    @Test
+    public void trySplitShouldSplitTheSpliteratorInTwoAlmostEqualSizedSpliteratorsAfterAdvancingOnce() {
+        Spliterator<Integer> spliterator = createNewSpliterator();
+        spliterator.tryAdvance(Integer::longValue);
+        Spliterator<Integer> spliterator2 = spliterator.trySplit();
+        assertEquals(THREE, spliterator.estimateSize());
+        assertEquals(2, spliterator2.estimateSize());
+    }
+
+    /**
+     * Verifies that trySplit returns null if there's only one element left.
+     */
+    @Test
+    public void trySplitShouldReturnNullIfOnlyOneElementLeft() {
+        Spliterator<Integer> spliterator = createNewSpliterator();
+        for (int i = 0; i < FIVE; i++) {
+            spliterator.tryAdvance(Integer::longValue);
+        }
+        assertNull(spliterator.trySplit());
+        assertEquals(1, spliterator.estimateSize());
+    }
+
+    /**
+     * Verifies that trySplit returns a spliterator starting with the right element.
+     */
+    @Test
+    public void trySplitShouldReturnASpliteratorStartingAtTheCorrectElement() {
+        Spliterator<Integer> spliterator = createNewSpliterator();
+        Spliterator<Integer> spliterator2 = spliterator.trySplit();
+        StringBuffer sb = new StringBuffer();
+        spliterator2.tryAdvance((x) -> sb.append(x));
+        assertEquals("1", sb.toString());
+    }
+
+    /**
+     * Verifies that trySplit moves the current spliterator to the right element.
+     */
+    @Test
+    public void trySplitShouldMoveSpliteratorToTheCorrectElement() {
+        Spliterator<Integer> spliterator = createNewSpliterator();
+        spliterator.trySplit();
+        StringBuffer sb = new StringBuffer();
+        spliterator.tryAdvance((x) -> sb.append(x));
+        assertEquals("4", sb.toString());
+    }
+
+    /**
+     * Verifies that a default spliterator has the correct characteristics.
+     */
+    @Test
+    public void characteristicsShouldReturnTheDefaultCharacteristicsByDefault() {
+        assertEquals(Spliterator.SIZED | Spliterator.SUBSIZED, createNewSpliterator().characteristics());
+    }
+
+    /**
+     * Verifies that additional characteristics are added to the spliterator.
+     */
+    @Test
+    public void additionalCharacteristicsShouldBeAddedToTheSpliterator() {
+        assertEquals(Spliterator.SIZED | Spliterator.SORTED | Spliterator.SUBSIZED,
+                new ArraySpliterator<Integer>(ARRAY123456, Spliterator.SORTED).characteristics());
+
+    }
+}
