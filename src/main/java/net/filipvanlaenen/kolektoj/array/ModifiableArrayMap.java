2c91dd0586c30bdfa9cfdaeaa8e0e645939c5360
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-10-10 09:28:15 +0200

    Added an implementation ModifiableArrayMap

--- src/main/java/net/filipvanlaenen/kolektoj/array/ModifiableArrayMap.java
+++ src/main/java/net/filipvanlaenen/kolektoj/array/ModifiableArrayMap.java
@@ -5,51 +5,67 @@ import java.util.Iterator;
 import java.util.Spliterator;
 
 import net.filipvanlaenen.kolektoj.Collection;
-import net.filipvanlaenen.kolektoj.Map;
 import net.filipvanlaenen.kolektoj.ModifiableCollection;
+import net.filipvanlaenen.kolektoj.ModifiableMap;
 
 /**
- * An array backed implementation of the {@link net.filipvanlaenen.kolektoj.Map} interface.
+ * An array backed implementation of the {@link net.filipvanlaenen.kolektoj.ModifiableMap} interface.
  *
  * @param <K> The key type.
  * @param <V> The value type.
  */
-public final class ArrayMap<K, V> implements Map<K, V> {
+public final class ModifiableArrayMap<K, V> implements ModifiableMap<K, V> {
     /**
      * The ratio by which the number of entries should be multiplied to construct the hashed array.
      */
     private static final int HASHING_RATIO = 3;
     /**
+     * The minimal ratio between the size of the map and the size of the hashed array.
+     */
+    private static final int MINIMAL_HASHING_RATIO = 2;
+    /**
+     * The stride for resizing the elements array.
+     */
+    private static final int STRIDE = 5;
+    /**
      * An array with the entries.
      */
-    private final Entry<K, V>[] entries;
+    private Entry<K, V>[] entries;
     /**
      * A hashed array with the entries.
      */
-    private final Entry<K, V>[] hashedEntries;
+    private Entry<K, V>[] hashedEntries;
     /**
      * The size of the hashed array with the entries.
      */
-    private final int hashedEntriesSize;
+    private int hashedEntriesSize;
     /**
-     * A collection with the keys, initialized lazily.
+     * A collection with the keys.
      */
-    private Collection<K> keys;
+    private ModifiableCollection<K> keys;
     /**
-     * A collection with the values, initialized lazily.
+     * The size of the map.
      */
-    private Collection<V> values;
+    private int size;
+    /**
+     * A collection with the values.
+     */
+    private ModifiableCollection<V> values;
 
     /**
      * Constructor taking the entries as its parameter.
      *
      * @param entries The entries for the map.
      */
-    public ArrayMap(final Entry<K, V>... entries) {
+    public ModifiableArrayMap(final Entry<K, V>... entries) {
         this.entries = entries.clone();
+        keys = new ModifiableArrayCollection<K>();
+        values = new ModifiableArrayCollection<V>();
         hashedEntriesSize = entries.length * HASHING_RATIO;
         Entry<K, V>[] hashedArray = createNewArray(hashedEntriesSize);
         for (Entry<K, V> entry : entries) {
+            keys.add(entry.key());
+            values.add(entry.value());
             int i = entry.key().hashCode() % hashedEntriesSize;
             while (hashedArray[i] != null) {
                 i = (i + 1) % hashedEntriesSize;
@@ -57,6 +73,26 @@ public final class ArrayMap<K, V> implements Map<K, V> {
             hashedArray[i] = entry;
         }
         this.hashedEntries = hashedArray;
+    }
+
+    @Override
+    public boolean add(final K key, final V value) {
+        Entry<K, V> entry = new Entry<K, V>(key, value);
+        if (size == entries.length) {
+            resizeEntriesTo(entries.length + STRIDE);
+        }
+        entries[size++] = entry;
+        if (size * MINIMAL_HASHING_RATIO > hashedEntriesSize) {
+            resizeHashedEntriesTo(size * HASHING_RATIO);
+        }
+        int i = key.hashCode() % hashedEntriesSize;
+        while (hashedEntries[i] != null) {
+            i = (i + 1) % hashedEntriesSize;
+        }
+        hashedEntries[i] = entry;
+        keys.add(key);
+        values.add(value);
+        return true;
     }
 
     @Override
@@ -89,20 +125,6 @@ public final class ArrayMap<K, V> implements Map<K, V> {
     private Entry<K, V>[] createNewArray(final int length) {
         Class<Entry<K, V>[]> clazz = (Class<Entry<K, V>[]>) entries.getClass();
         return (Entry<K, V>[]) Array.newInstance(clazz.getComponentType(), length);
-    }
-
-    @Override
-    public Entry<K, V> get() throws IndexOutOfBoundsException {
-        if (entries.length == 0) {
-            throw new IndexOutOfBoundsException("Cannot return an entry from an empty map.");
-        } else {
-            return entries[0];
-        }
-    }
-
-    @Override
-    public Iterator<Entry<K, V>> iterator() {
-        return new ArrayIterator<Entry<K, V>>(entries);
     }
 
     /**
@@ -141,6 +163,15 @@ public final class ArrayMap<K, V> implements Map<K, V> {
     }
 
     @Override
+    public Entry<K, V> get() throws IndexOutOfBoundsException {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Cannot return an entry from an empty map.");
+        } else {
+            return entries[0];
+        }
+    }
+
+    @Override
     public V get(final K key) throws IllegalArgumentException {
         int index = findFirstIndexForKey(key);
         if (index == -1) {
@@ -168,31 +199,63 @@ public final class ArrayMap<K, V> implements Map<K, V> {
 
     @Override
     public Collection<K> getKeys() {
-        if (keys == null) {
-            ModifiableCollection<K> result = ModifiableCollection.empty();
-            for (Entry<K, V> entry : entries) {
-                result.add(entry.key());
-            }
-            keys = Collection.of(result.toArray());
-        }
-        return keys;
+        // TODO: return keys.clone() instead
+        return new ArrayCollection<K>(keys.toArray());
     }
 
     @Override
     public Collection<V> getValues() {
-        if (values == null) {
-            ModifiableCollection<V> result = ModifiableCollection.empty();
-            for (Entry<K, V> entry : entries) {
-                result.add(entry.value());
+        // TODO: return values.clone() instead
+        return new ArrayCollection<V>(values.toArray());
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new ArrayIterator<Entry<K, V>>(entries);
+    }
+
+    @Override
+    public V remove(final K key) throws IllegalArgumentException {
+        // TODO: Find the entry and remove it from entries, hashedEntries, keys and values.
+        // TODO: If needed, shrink entries and hashedEntries.
+        // TODO: If needed, rehash hashedEntries so that the findFirstIndex methods continue to work, i.e. rehash if the
+        // next entry in the hashed array isn't null.
+        return null;
+    }
+
+    /**
+     * Resizes the entries array to the new length. It is assumed that the new length is not less than the current size.
+     *
+     * @param newLength The new length for the entries array.
+     */
+    private void resizeEntriesTo(final int newLength) {
+        Entry<K, V>[] newElements = createNewArray(newLength);
+        System.arraycopy(entries, 0, newElements, 0, size);
+        entries = newElements;
+    }
+
+    /**
+     * Resizes the hashed entries array to the new length. It is assumed that the new length is not less than the
+     * current size.
+     *
+     * @param newLength The new length for the hashed entries array.
+     */
+    private void resizeHashedEntriesTo(final int newLength) {
+        hashedEntriesSize = newLength;
+        Entry<K, V>[] hashedArray = createNewArray(hashedEntriesSize);
+        for (Entry<K, V> entry : entries) {
+            int i = entry.key().hashCode() % hashedEntriesSize;
+            while (hashedArray[i] != null) {
+                i = (i + 1) % hashedEntriesSize;
             }
-            values = Collection.of(result.toArray());
+            hashedArray[i] = entry;
         }
-        return values;
+        this.hashedEntries = hashedArray;
     }
 
     @Override
     public int size() {
-        return entries.length;
+        return size;
     }
 
     @Override
@@ -202,6 +265,8 @@ public final class ArrayMap<K, V> implements Map<K, V> {
 
     @Override
     public Entry<K, V>[] toArray() {
-        return entries.clone();
+        Entry<K, V>[] result = createNewArray(size);
+        System.arraycopy(entries, 0, result, 0, size);
+        return result;
     }
 }
