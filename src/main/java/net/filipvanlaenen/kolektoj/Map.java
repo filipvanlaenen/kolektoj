dbff5ed04d649cb74868ffbb8dcc82e18bf6d580
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-10-12 13:59:44 +0200

    Added unit tests to Map

--- src/main/java/net/filipvanlaenen/kolektoj/Map.java
+++ src/main/java/net/filipvanlaenen/kolektoj/Map.java
@@ -2,7 +2,6 @@ package net.filipvanlaenen.kolektoj;
 
 import net.filipvanlaenen.kolektoj.Map.Entry;
 import net.filipvanlaenen.kolektoj.hash.HashMap;
-import net.filipvanlaenen.kolektoj.hash.ModifiableHashMap;
 
 /**
  * Interface defining the signature for all maps.
@@ -45,14 +44,47 @@ public interface Map<K, V> extends Collection<Entry<K, V>> {
         return new HashMap<K, V>(entries);
     }
 
+    /**
+     * Returns a new map containing an entry with the key and the value.
+     *
+     * @param <K>   The key type.
+     * @param <V>   The value type.
+     * @param key   The key for the entry.
+     * @param value The value for the entry.
+     * @return A new map containing an entry with the key and the value.
+     */
     static <K, V> Map<K, V> of(final K key, final V value) {
         return new HashMap<K, V>(new Entry<K, V>(key, value));
     }
 
+    /**
+     * Returns a new map containing two entries using the provided keys and values.
+     *
+     * @param <K>    The key type.
+     * @param <V>    The value type.
+     * @param key1   The first key for the entry.
+     * @param value1 The first value for the entry.
+     * @param key2   The second key for the entry.
+     * @param value2 The second value for the entry.
+     * @return A new map containing two entries using the provided keys and values.
+     */
     static <K, V> Map<K, V> of(final K key1, final V value1, final K key2, final V value2) {
         return new HashMap<K, V>(new Entry<K, V>(key1, value1), new Entry<K, V>(key2, value2));
     }
 
+    /**
+     * Returns a new map containing three entries using the provided keys and values.
+     *
+     * @param <K>    The key type.
+     * @param <V>    The value type.
+     * @param key1   The first key for the entry.
+     * @param value1 The first value for the entry.
+     * @param key2   The second key for the entry.
+     * @param value2 The second value for the entry.
+     * @param key3   The third key for the entry.
+     * @param value3 The third value for the entry.
+     * @return A new map containing three entries using the provided keys and values.
+     */
     static <K, V> Map<K, V> of(final K key1, final V value1, final K key2, final V value2, final K key3,
             final V value3) {
         return new HashMap<K, V>(new Entry<K, V>(key1, value1), new Entry<K, V>(key2, value2),
--- src/test/java/net/filipvanlaenen/kolektoj/MapTest.java
+++ src/test/java/net/filipvanlaenen/kolektoj/MapTest.java
@@ -1,14 +1,22 @@
 package net.filipvanlaenen.kolektoj;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import net.filipvanlaenen.kolektoj.Map.Entry;
+
 /**
  * Unit tests on the {@link net.filipvanlaenen.kolektoj.Map} class.
  */
 public class MapTest {
+    /**
+     * The magic number three.
+     */
+    private static final int THREE = 3;
+
     /**
      * Verifies that an empty map is empty.
      */
@@ -23,5 +31,38 @@ public class MapTest {
     @Test
     public void isEmptyShouldReturnFalseForAMapContainingAnElement() {
         assertFalse(Map.of(new Map.Entry<Integer, Integer>(1, 1)).isEmpty());
+    }
+
+    /**
+     * Verifies that a map constructed with one key and value contains an entry with the key and value.
+     */
+    @Test
+    public void ofShouldConstructAMapContainingAnEntryWithTheKeyAndValue() {
+        Map<Integer, String> actual = Map.of(1, "one");
+        assertEquals(1, actual.size());
+        assertTrue(actual.contains(new Entry<Integer, String>(1, "one")));
+    }
+
+    /**
+     * Verifies that a map constructed with two keys and values contains two entries with the keys and values.
+     */
+    @Test
+    public void ofShouldConstructAMapContainingTwoEntriesWithTheKeysAndValues() {
+        Map<Integer, String> actual = Map.of(1, "one", 2, "two");
+        assertEquals(2, actual.size());
+        assertTrue(actual.contains(new Entry<Integer, String>(1, "one")));
+        assertTrue(actual.contains(new Entry<Integer, String>(2, "two")));
+    }
+
+    /**
+     * Verifies that a map constructed with three keys and values contains two entries with the keys and values.
+     */
+    @Test
+    public void ofShouldConstructAMapContainingThreeEntriesWithTheKeysAndValues() {
+        Map<Integer, String> actual = Map.of(1, "one", 2, "two", THREE, "three");
+        assertEquals(THREE, actual.size());
+        assertTrue(actual.contains(new Entry<Integer, String>(1, "one")));
+        assertTrue(actual.contains(new Entry<Integer, String>(2, "two")));
+        assertTrue(actual.contains(new Entry<Integer, String>(THREE, "three")));
     }
 }
