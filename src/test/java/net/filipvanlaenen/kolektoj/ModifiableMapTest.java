8e0429080d5cfbec8085d7760638394f55c40d62
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-10-12 14:44:53 +0200

    Added unit tests to ModifiableMapTest

--- src/test/java/net/filipvanlaenen/kolektoj/ModifiableMapTest.java
+++ src/test/java/net/filipvanlaenen/kolektoj/ModifiableMapTest.java
@@ -2,6 +2,7 @@ package net.filipvanlaenen.kolektoj;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -64,5 +65,52 @@ public class ModifiableMapTest {
         assertTrue(actual.contains(new Entry<Integer, String>(1, "one")));
         assertTrue(actual.contains(new Entry<Integer, String>(2, "two")));
         assertTrue(actual.contains(new Entry<Integer, String>(THREE, "three")));
+    }
+
+    /**
+     * Verifies that put returns null when the key is absent in the map.
+     */
+    @Test
+    public void putShouldReturnNullIfTheKeyIsNew() {
+        assertNull(ModifiableMap.empty().put(1, "one"));
+    }
+
+    /**
+     * Verifies that put returns the previous value if the key is present in the map.
+     */
+    @Test
+    public void putShouldReturnPreviousValueIfTheKeyIsPresent() {
+        assertEquals("one", ModifiableMap.of(1, "one").put(1, "another"));
+    }
+
+    /**
+     * Verifies that put adds an entry if the key is absent in the map.
+     */
+    @Test
+    public void putShouldAddEntryIfTheKeyIsAbsent() {
+        ModifiableMap<Integer, String> map = ModifiableMap.empty();
+        map.put(1, "one");
+        assertTrue(map.contains(new Entry<Integer, String>(1, "one")));
+    }
+
+    /**
+     * Verifies that put updates an entry if the key is present in the map.
+     */
+    @Test
+    public void putShouldUpdateEntryIfTheKeyIsPresent() {
+        ModifiableMap<Integer, String> map = ModifiableMap.of(1, "another");
+        map.put(1, "one");
+        assertTrue(map.contains(new Entry<Integer, String>(1, "one")));
+    }
+
+    /**
+     * Verifies that putAll puts all entries in the provided map.
+     */
+    @Test
+    public void putAllShouldPutAllEntriesFromTheMap() {
+        ModifiableMap<Integer, String> map = ModifiableMap.of(1, "another");
+        map.putAll(Map.of(1, "one", 2, "two"));
+        assertTrue(map.contains(new Entry<Integer, String>(1, "one")));
+        assertTrue(map.contains(new Entry<Integer, String>(2, "two")));
     }
 }
