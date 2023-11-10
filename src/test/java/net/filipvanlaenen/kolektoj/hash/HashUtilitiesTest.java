f7a3eda380e116b4cdf8f157636887a77c4f4681
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-11-10 14:03:02 +0100

    Added unit tests to HashUtilitiesTest

--- src/test/java/net/filipvanlaenen/kolektoj/hash/HashUtilitiesTest.java
+++ src/test/java/net/filipvanlaenen/kolektoj/hash/HashUtilitiesTest.java
@@ -1,17 +1,50 @@
 package net.filipvanlaenen.kolektoj.hash;
 
+import static net.filipvanlaenen.kolektoj.Map.KeyAndValueCardinality.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import net.filipvanlaenen.kolektoj.Map;
+import net.filipvanlaenen.kolektoj.Map.Entry;
 
 /**
  * Unit tests on the {@link net.filipvanlaenen.kolektoj.hash.HashUtilities} class.
  */
 public class HashUtilitiesTest {
     /**
+     * The magic number three.
+     */
+    private static final int THREE = 3;
+    /**
+     * The magic number four.
+     */
+    private static final int FOUR = 4;
+    /**
      * The magic number five.
      */
     private static final int FIVE = 5;
+    /**
+     * An entry with key 1 and value one.
+     */
+    private static final Entry<Integer, String> ENTRY1 = new Entry<Integer, String>(1, "one");
+    /**
+     * Another entry with key 1 and value one.
+     */
+    private static final Entry<Integer, String> OTHER_ENTRY1 = new Entry<Integer, String>(1, "one");
+    /**
+     * Another entry with key 1 and value bis.
+     */
+    private static final Entry<Integer, String> ENTRY1BIS = new Entry<Integer, String>(1, "bis");
+    /**
+     * An entry with key 2 and value two.
+     */
+    private static final Entry<Integer, String> ENTRY2 = new Entry<Integer, String>(2, "two");
+    /**
+     * An entry with key 3 and value three.
+     */
+    private static final Entry<Integer, String> ENTRY3 = new Entry<Integer, String>(3, "three");
 
     /**
      * Class with an integer field that is returned as its hash code.
@@ -64,5 +97,59 @@ public class HashUtilitiesTest {
     @Test
     public void hashShouldBePositive() {
         assertEquals(1, HashUtilities.hash(new IntegerFieldObject(-1), 2));
+    }
+
+    /**
+     * Verifies that the entries are populated correctly when keys should be distinct. The method is tested through the
+     * constructor of <code>HashMap</code>.
+     */
+    @Test
+    public void populateMapFromEntriesShouldProduceCorrectEntriesForDistinctKeys() {
+        Map<Integer, String> map =
+                new HashMap<Integer, String>(DISTINCT_KEYS, ENTRY1, ENTRY1BIS, OTHER_ENTRY1, ENTRY2, ENTRY3);
+        assertEquals(THREE, map.size());
+    }
+
+    /**
+     * Verifies that the entries are populated correctly when keys can be duplicate but the values must be distinct. The
+     * method is tested through the constructor of <code>HashMap</code>.
+     */
+    @Test
+    public void populateMapFromEntriesShouldProduceCorrectEntriesForDuplicateKeysWithDistinctValues() {
+        Map<Integer, String> map = new HashMap<Integer, String>(DUPLICATE_KEYS_WITH_DISTINCT_VALUES, ENTRY1, ENTRY1BIS,
+                OTHER_ENTRY1, ENTRY2, ENTRY3);
+        assertEquals(FOUR, map.size());
+    }
+
+    /**
+     * Verifies that the entries are populated correctly when keys and the values can be duplicate. The method is tested
+     * through the constructor of <code>HashMap</code>.
+     */
+    @Test
+    public void populateMapFromEntriesShouldProduceCorrectEntriesForDuplicateKeysAndValues() {
+        Map<Integer, String> map = new HashMap<Integer, String>(DUPLICATE_KEYS_WITH_DUPLICATE_VALUES, ENTRY1, ENTRY1BIS,
+                OTHER_ENTRY1, ENTRY2, ENTRY3);
+        assertEquals(FIVE, map.size());
+    }
+
+    /**
+     * Verifies that the hashed entries are populated correctly when keys and the values can be duplicate. The method is
+     * tested through the constructor of <code>HashMap</code>.
+     */
+    @Test
+    public void populateMapFromEntriesShouldProduceCorrectHashedEntriesForDuplicateKeysAndValues() {
+        Map<Integer, String> map = new HashMap<Integer, String>(DUPLICATE_KEYS_WITH_DUPLICATE_VALUES, ENTRY1, ENTRY1BIS,
+                OTHER_ENTRY1, ENTRY2, ENTRY3);
+        assertEquals(THREE, map.getAll(1).size());
+    }
+
+    /**
+     * Verifies that trying to populate a map with an entry that's null throws an <code>IllegalArgumentException</code>.
+     */
+    @Test
+    public void populateMapFromEntriesShouldThroughIllegalArgumentExceptionForNullEntry() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new HashMap<Integer, String>(ENTRY1, null, ENTRY2, ENTRY3));
+        assertEquals("Map entries can't be null.", exception.getMessage());
     }
 }
