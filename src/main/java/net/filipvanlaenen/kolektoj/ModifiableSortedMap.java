714bd4dccf0e0905f1f7a8febc0c05d7bb9fb8d5
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-03-12 11:14:03 +0100

    Issue #38: Added the empty factory method to ModifiableSortedMap

--- src/main/java/net/filipvanlaenen/kolektoj/ModifiableSortedMap.java
+++ src/main/java/net/filipvanlaenen/kolektoj/ModifiableSortedMap.java
@@ -1,6 +1,9 @@
 package net.filipvanlaenen.kolektoj;
 
+import java.util.Comparator;
+
 import net.filipvanlaenen.kolektoj.Map.Entry;
+import net.filipvanlaenen.kolektoj.sortedtree.ModifiableSortedTreeMap;
 
 /**
  * Interface defining the signature for all modifiable sorted maps.
@@ -10,4 +13,14 @@ import net.filipvanlaenen.kolektoj.Map.Entry;
  */
 public interface ModifiableSortedMap<K, V>
         extends Collection<Entry<K, V>>, ModifiableMap<K, V>, UpdatableSortedMap<K, V> {
+    /**
+     * Returns a new empty modifiable sorted map.
+     *
+     * @param <K> The key type.
+     * @param <V> The value type.
+     * @return A new empty modifiable sorted map.
+     */
+    static <K, V> ModifiableSortedMap<K, V> empty(final Comparator<K> comparator) {
+        return new ModifiableSortedTreeMap<K, V>(comparator);
+    }
 }
