4927f75ccbb667f3dac1dde29ea52bb398fc308f
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-03-26 13:49:16 +0100

    Issue #6, #32, #33 and #39: Added unit tests to Node and resolved some Checkstyle violations

--- src/main/java/net/filipvanlaenen/kolektoj/sortedtree/Node.java
+++ src/main/java/net/filipvanlaenen/kolektoj/sortedtree/Node.java
@@ -7,63 +7,143 @@ package net.filipvanlaenen.kolektoj.sortedtree;
  * @param <C> The content type.
  */
 class Node<K, C> {
+    /**
+     * The content of the node.
+     */
     private C content;
+    /**
+     * The height of the node.
+     */
     private int height;
+    /**
+     * The key of the node.
+     */
     private K key;
+    /**
+     * The left child of the node.
+     */
     private Node<K, C> leftChild;
+    /**
+     * The right child of the node.
+     */
     private Node<K, C> rightChild;
 
+    /**
+     * Constructor taking the key as its parameter. Sets the content to <code>null</code>.
+     *
+     * @param key The key for the node.
+     */
     Node(final K key) {
         this(key, null);
     }
 
+    /**
+     * Constructor taking the key and the content as its parameters.
+     *
+     * @param key     The key for the node.
+     * @param content The content for the node.
+     */
     Node(final K key, final C content) {
         this.key = key;
         this.content = content;
     }
 
+    /**
+     * Returns the content of the node.
+     *
+     * @return The content of the node.
+     */
     C getContent() {
         return content;
     }
 
+    /**
+     * Returns the height of the node.
+     *
+     * @return The height of the node.
+     */
     int getHeight() {
         return height;
     }
 
+    /**
+     * Returns the key of the node.
+     *
+     * @return The key of the node.
+     */
     K getKey() {
         return key;
     }
 
+    /**
+     * Returns the left child of the node.
+     *
+     * @return The left child of the node.
+     */
     Node<K, C> getLeftChild() {
         return leftChild;
     }
 
+    /**
+     * Returns the leftmost child of the node. If the node has no left child, the node itself will be returned.
+     *
+     * @return The leftmost child of the node, or the node itself if it has no left child.
+     */
     Node<K, C> getLeftmostChild() {
         return leftChild == null ? this : leftChild.getLeftmostChild();
     }
 
+    /**
+     * Returns the right child of the node.
+     *
+     * @return The right child of the node.
+     */
     Node<K, C> getRightChild() {
         return rightChild;
     }
 
-    void setHeight(final int height) {
-        this.height = height;
-    }
-
-    void setKey(final K key) {
-        this.key = key;
-    }
-
-    void setLeftChild(final Node<K, C> node) {
-        leftChild = node;
-    }
-
-    void setRightChild(final Node<K, C> node) {
-        rightChild = node;
-    }
-
+    /**
+     * Returns the size of the node.
+     *
+     * @return The size of the node.
+     */
     int getSize() {
         return 1 + (leftChild == null ? 0 : leftChild.getSize()) + (rightChild == null ? 0 : rightChild.getSize());
     }
 
+    /**
+     * Sets the height of the node.
+     *
+     * @param height The height of the node.
+     */
+    void setHeight(final int height) {
+        this.height = height;
+    }
+
+    /**
+     * Sets the key of the node.
+     *
+     * @param key The key of the node.
+     */
+    void setKey(final K key) {
+        this.key = key;
+    }
+
+    /**
+     * Sets the left child of the node.
+     *
+     * @param leftChild The left child of the node.
+     */
+    void setLeftChild(final Node<K, C> leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    /**
+     * Sets the right child of the node.
+     *
+     * @param rightChild The right child of the node.
+     */
+    void setRightChild(final Node<K, C> rightChild) {
+        this.rightChild = rightChild;
+    }
 }
--- src/test/java/net/filipvanlaenen/kolektoj/sortedtree/NodeTest.java
+++ src/test/java/net/filipvanlaenen/kolektoj/sortedtree/NodeTest.java
@@ -1,19 +1,135 @@
 package net.filipvanlaenen.kolektoj.sortedtree;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Unit tests on the {@link net.filipvanlaenen.kolektoj.sortedtree.Node} class.
+ */
 public class NodeTest {
-    @Test
-    public void getKeyShouldBeWiredCorrectlyToConstructor() {
-        Node<Integer, Integer> node = new Node<Integer, Integer>(1, 2);
-        assertEquals(1, node.getKey());
+    /**
+     * The magic number three.
+     */
+    private static final int THREE = 3;
+    /**
+     * A node to run unit tests on.
+     */
+    private static final Node<Integer, Integer> NODE12 = createTestNode12();
+
+    /**
+     * Factory method to create a test node.
+     *
+     * @return A node with key 1 and content 2.
+     */
+    private static Node<Integer, Integer> createTestNode12() {
+        return new Node<Integer, Integer>(1, 2);
     }
 
+    /**
+     * Verifies that the key is wired correctly to the constructor.
+     */
+    @Test
+    public void getKeyShouldBeWiredCorrectlyToConstructor() {
+        assertEquals(1, NODE12.getKey());
+    }
+
+    /**
+     * Verifies that the content is wired correctly to the constructor.
+     */
     @Test
     public void getContentShouldBeWiredCorrectlyToConstructor() {
-        Node<Integer, Integer> node = new Node<Integer, Integer>(1, 2);
-        assertEquals(2, node.getContent());
+        assertEquals(2, NODE12.getContent());
+    }
+
+    /**
+     * Verifies that the getter for the height is wired correctly to the setter.
+     */
+    @Test
+    public void getHeightShouldBeWiredCorrectyToGetHeight() {
+        Node<Integer, Integer> node = createTestNode12();
+        node.setHeight(THREE);
+        assertEquals(THREE, node.getHeight());
+    }
+
+    /**
+     * Verifies that by default, the left child is <code>null</code>.
+     */
+    @Test
+    public void getLeftChildShouldReturnNullByDefault() {
+        assertNull(NODE12.getLeftChild());
+    }
+
+    /**
+     * Verifies that the getter for the left child is wired correctly to the setter.
+     */
+    @Test
+    public void getLeftChildShouldBeWiredCorrectlyToSetLeftChild() {
+        Node<Integer, Integer> parent = createTestNode12();
+        Node<Integer, Integer> child = createTestNode12();
+        parent.setLeftChild(child);
+        assertEquals(child, parent.getLeftChild());
+    }
+
+    /**
+     * Verifies that by default, the right child is <code>null</code>.
+     */
+    @Test
+    public void getRightChildShouldReturnNullByDefault() {
+        assertNull(NODE12.getRightChild());
+    }
+
+    /**
+     * Verifies that the getter for the right child is wired correctly to the setter.
+     */
+    @Test
+    public void getRightChildShouldBeWiredCorrectlyToSetRightChild() {
+        Node<Integer, Integer> parent = createTestNode12();
+        Node<Integer, Integer> child = createTestNode12();
+        parent.setRightChild(child);
+        assertEquals(child, parent.getRightChild());
+    }
+
+    /**
+     * Verifies that by default, the leftmost child is the node itself.
+     */
+    @Test
+    public void getLeftmostChildShouldReturnNullByDefault() {
+        assertEquals(NODE12, NODE12.getLeftmostChild());
+    }
+
+    /**
+     * Verifies that the getter for the leftmost child navigates to the leftmost grandchild.
+     */
+    @Test
+    public void getLeftmostChildShouldReturnLeftmostGrandchild() {
+        Node<Integer, Integer> parent = createTestNode12();
+        Node<Integer, Integer> child = createTestNode12();
+        Node<Integer, Integer> grandchild = createTestNode12();
+        parent.setLeftChild(child);
+        child.setLeftChild(grandchild);
+        assertEquals(grandchild, parent.getLeftmostChild());
+    }
+
+    /**
+     * Verifies that by default, the size is one.
+     */
+    @Test
+    public void getSizeShouldReturnOneByDefault() {
+        assertEquals(1, NODE12.getSize());
+    }
+
+    /**
+     * Verifies that the size is three for a node with both a left and a right child.
+     */
+    @Test
+    public void getSizeShouldReturnThreeForNodeWithLeftAndRightChild() {
+        Node<Integer, Integer> parent = createTestNode12();
+        Node<Integer, Integer> leftChild = createTestNode12();
+        Node<Integer, Integer> rightChild = createTestNode12();
+        parent.setLeftChild(leftChild);
+        parent.setRightChild(rightChild);
+        assertEquals(THREE, parent.getSize());
     }
 }
