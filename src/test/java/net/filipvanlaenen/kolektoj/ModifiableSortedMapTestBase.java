62d13707858b4f457555ee96908296a5fe7d7e15
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-04-09 09:46:24 +0200

    Issue #33: Resolved a Checkstyle violation in ModifiableSortedMapTestBase

--- src/test/java/net/filipvanlaenen/kolektoj/ModifiableSortedMapTestBase.java
+++ src/test/java/net/filipvanlaenen/kolektoj/ModifiableSortedMapTestBase.java
@@ -1,5 +1,10 @@
 package net.filipvanlaenen.kolektoj;
 
+/**
+ * Unit tests on implementations of the {@link net.filipvanlaenen.kolektoj.ModifiableSortedMap} interface.
+ *
+ * @param <T> The subclass type to be tested.
+ */
 public abstract class ModifiableSortedMapTestBase<T extends ModifiableSortedMap<Integer, String>>
         extends MapTestBase<T> {
 }
