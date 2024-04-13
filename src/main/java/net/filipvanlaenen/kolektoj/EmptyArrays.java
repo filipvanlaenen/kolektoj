551e831dfe24119df3f81a98469eaf6fd86719be
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-04-13 18:49:20 +0200

    Issue #47: Ensured type safety during the construction of collections

--- src/main/java/net/filipvanlaenen/kolektoj/EmptyArrays.java
+++ src/main/java/net/filipvanlaenen/kolektoj/EmptyArrays.java
@@ -1,6 +1,19 @@
 package net.filipvanlaenen.kolektoj;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class EmptyArrays {
-    public static final Object[] INTEGERS = new Integer[] {};
+    public static final BigDecimal[] BIG_DECIMALS = new BigDecimal[] {};
+    public static final BigInteger[] BIG_INTEGERS = new BigInteger[] {};
+    public static final Boolean[] BOOLEANS = new Boolean[] {};
+    public static final Byte[] BYTES = new Byte[] {};
+    public static final Character[] CHARACTERS = new Character[] {};
+    public static final Double[] DOUBLES = new Double[] {};
+    public static final Float[] FLOATS = new Float[] {};
+    public static final Integer[] INTEGERS = new Integer[] {};
+    public static final Long[] LONGS = new Long[] {};
     public static final Object[] OBJECTS = new Object[] {};
+    public static final Short[] SHORTS = new Short[] {};
+    public static final String[] STRINGS = new String[] {};
 }
