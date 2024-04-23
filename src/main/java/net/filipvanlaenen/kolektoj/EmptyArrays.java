29dc829227e9b18cce290555e7c65bac6c30945b
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-04-23 13:06:39 +0200

    Issue #47: Resolved some Checkstyle violations

--- src/main/java/net/filipvanlaenen/kolektoj/EmptyArrays.java
+++ src/main/java/net/filipvanlaenen/kolektoj/EmptyArrays.java
@@ -3,17 +3,58 @@ package net.filipvanlaenen.kolektoj;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-public class EmptyArrays {
+/**
+ * Utility class containing empty arrays for some basic types.
+ */
+public final class EmptyArrays {
+    /**
+     * An empty BigDecimal array.
+     */
     public static final BigDecimal[] BIG_DECIMALS = new BigDecimal[] {};
+    /**
+     * An empty BigInteger array.
+     */
     public static final BigInteger[] BIG_INTEGERS = new BigInteger[] {};
+    /**
+     * An empty Boolean array.
+     */
     public static final Boolean[] BOOLEANS = new Boolean[] {};
+    /**
+     * An empty Byte array.
+     */
     public static final Byte[] BYTES = new Byte[] {};
+    /**
+     * An empty Character array.
+     */
     public static final Character[] CHARACTERS = new Character[] {};
+    /**
+     * An empty Double array.
+     */
     public static final Double[] DOUBLES = new Double[] {};
+    /**
+     * An empty Float array.
+     */
     public static final Float[] FLOATS = new Float[] {};
+    /**
+     * An empty Integer array.
+     */
     public static final Integer[] INTEGERS = new Integer[] {};
+    /**
+     * An empty Long array.
+     */
     public static final Long[] LONGS = new Long[] {};
-    public static final Object[] OBJECTS = new Object[] {};
+    /**
+     * An empty Short array.
+     */
     public static final Short[] SHORTS = new Short[] {};
+    /**
+     * An empty String array.
+     */
     public static final String[] STRINGS = new String[] {};
+
+    /**
+     * Private constructor to avoid instantiation of this utility class.
+     */
+    private EmptyArrays() {
+    }
 }
