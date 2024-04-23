a1e6e858ffa0cf1a8c3be29a3600fd6d16ac08e9
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-04-23 12:05:28 +0200

    Issue #47: Corrected class casting issue

--- src/main/java/net/filipvanlaenen/kolektoj/sortedtree/SortedTreeMap.java
+++ src/main/java/net/filipvanlaenen/kolektoj/sortedtree/SortedTreeMap.java
@@ -22,209 +22,213 @@ import net.filipvanlaenen.kolektoj.array.ModifiableArrayCollection;
 import net.filipvanlaenen.kolektoj.array.SortedArrayCollection;
 
 /**
- * An implementation of the {@link net.filipvanlaenen.kolektoj.SortedMap} interface backed by a sorted tree, in
- * particular an AVL tree.
+ * An implementation of the {@link net.filipvanlaenen.kolektoj.SortedMap}
+ * interface backed by a sorted tree, in particular an AVL tree.
  *
  * @param <K> The key type.
  * @param <V> The value type.
  */
 public final class SortedTreeMap<K, V> implements SortedMap<K, V> {
-    /**
-     * A sorted array with the entries.
-     */
-    private final Object[] entries;
-    /**
-     * The key and value cardinality.
-     */
-    private final KeyAndValueCardinality keyAndValueCardinality;
-    /**
-     * The comparator to use for comparing entries using the keys only in this sorted map.
-     */
-    private final Comparator<Entry<K, V>> entryByKeyComparator;
-    /**
-     * A sorted collection with the keys.
-     */
-    private final SortedCollection<K> keys;
-    /**
-     * The size of the collection.
-     */
-    private final int size;
-    /**
-     * The sorted tree with the entries.
-     */
-    private final SortedTree<K, Collection<V>> sortedTree;
-    /**
-     * A collection with the values.
-     */
-    private final Collection<V> values;
+	/**
+	 * A sorted array with the entries.
+	 */
+	private final Object[] entries;
+	/**
+	 * The key and value cardinality.
+	 */
+	private final KeyAndValueCardinality keyAndValueCardinality;
+	/**
+	 * The comparator to use for comparing entries using the keys only in this
+	 * sorted map.
+	 */
+	private final Comparator<Entry<K, V>> entryByKeyComparator;
+	/**
+	 * A sorted collection with the keys.
+	 */
+	private final SortedCollection<K> keys;
+	/**
+	 * The size of the collection.
+	 */
+	private final int size;
+	/**
+	 * The sorted tree with the entries.
+	 */
+	private final SortedTree<K, Collection<V>> sortedTree;
+	/**
+	 * A collection with the values.
+	 */
+	private final Collection<V> values;
 
-    /**
-     * Constructor taking the entries as its parameter.
-     *
-     * @param entries The entries for the map.
-     * @throws IllegalArgumentException Thrown if one of the entries is null.
-     */
-    public SortedTreeMap(final Comparator<K> comparator, final Entry<K, V>... entries) throws IllegalArgumentException {
-        this(DISTINCT_KEYS, comparator, entries);
-    }
+	/**
+	 * Constructor taking the entries as its parameter.
+	 *
+	 * @param entries The entries for the map.
+	 * @throws IllegalArgumentException Thrown if one of the entries is null.
+	 */
+	public SortedTreeMap(final Comparator<K> comparator, final Entry<K, V>... entries) throws IllegalArgumentException {
+		this(DISTINCT_KEYS, comparator, entries);
+	}
 
-    /**
-     * Constructor taking the key and value cardinality and the entries as its parameter.
-     *
-     * @param keyAndValueCardinality The key and value cardinality.
-     * @param entries                The entries for the map.
-     * @throws IllegalArgumentException Thrown if one of the entries is null.
-     */
-    public SortedTreeMap(final KeyAndValueCardinality keyAndValueCardinality, final Comparator<K> comparator,
-            final Entry<K, V>... entries) throws IllegalArgumentException {
-        if (entries == null) {
-            throw new IllegalArgumentException("Map entries can't be null.");
-        }
-        for (Entry<K, V> entry : entries) {
-            if (entry == null) {
-                throw new IllegalArgumentException("Map entries can't be null.");
-            }
-        }
-        this.entryByKeyComparator = new Comparator<Entry<K, V>>() {
-            @Override
-            public int compare(final Entry<K, V> e1, final Entry<K, V> e2) {
-                return comparator.compare(e1.key(), e2.key());
-            }
-        };
-        this.keyAndValueCardinality = keyAndValueCardinality;
-        if (keyAndValueCardinality == DISTINCT_KEYS) {
-            this.entries =
-                    ArrayUtilities.quicksort(ArrayUtilities.cloneDistinctElements(entries), entryByKeyComparator);
-        } else {
-            this.entries = ArrayUtilities.quicksort(entries, entryByKeyComparator);
-        }
-        size = this.entries.length;
+	/**
+	 * Constructor taking the key and value cardinality and the entries as its
+	 * parameter.
+	 *
+	 * @param keyAndValueCardinality The key and value cardinality.
+	 * @param entries                The entries for the map.
+	 * @throws IllegalArgumentException Thrown if one of the entries is null.
+	 */
+	public SortedTreeMap(final KeyAndValueCardinality keyAndValueCardinality, final Comparator<K> comparator,
+			final Entry<K, V>... entries) throws IllegalArgumentException {
+		if (entries == null) {
+			throw new IllegalArgumentException("Map entries can't be null.");
+		}
+		for (Entry<K, V> entry : entries) {
+			if (entry == null) {
+				throw new IllegalArgumentException("Map entries can't be null.");
+			}
+		}
+		this.entryByKeyComparator = new Comparator<Entry<K, V>>() {
+			@Override
+			public int compare(final Entry<K, V> e1, final Entry<K, V> e2) {
+				return comparator.compare(e1.key(), e2.key());
+			}
+		};
+		this.keyAndValueCardinality = keyAndValueCardinality;
+		if (keyAndValueCardinality == DISTINCT_KEYS) {
+			this.entries = ArrayUtilities.quicksort(ArrayUtilities.cloneDistinctElements(entries),
+					entryByKeyComparator);
+		} else {
+			this.entries = ArrayUtilities.quicksort(entries, entryByKeyComparator);
+		}
+		size = this.entries.length;
 
-        sortedTree = SortedTree.fromSortedEntryArray(comparator, keyAndValueCardinality, compact(this.entries));
-        ModifiableCollection<K> theKeys = new ModifiableSortedTreeCollection<K>(
-                keyAndValueCardinality == DISTINCT_KEYS ? DISTINCT_ELEMENTS : DUPLICATE_ELEMENTS, comparator);
-        ModifiableCollection<V> theValues = new ModifiableArrayCollection<V>();
-        for (Entry<K, V> entry : entries) {
-            theKeys.add(entry.key());
-            theValues.add(entry.value());
-        }
-        this.keys = new SortedArrayCollection<K>(comparator, theKeys);
-        this.values = new ArrayCollection<V>(theValues);
-    }
+		sortedTree = SortedTree.fromSortedEntryArray(comparator, keyAndValueCardinality, compact(this.entries));
+		ModifiableCollection<K> theKeys = new ModifiableSortedTreeCollection<K>(
+				keyAndValueCardinality == DISTINCT_KEYS ? DISTINCT_ELEMENTS : DUPLICATE_ELEMENTS, comparator);
+		ModifiableCollection<V> theValues = new ModifiableArrayCollection<V>();
+		for (Entry<K, V> entry : entries) {
+			theKeys.add(entry.key());
+			theValues.add(entry.value());
+		}
+		this.keys = new SortedArrayCollection<K>(comparator, theKeys);
+		this.values = new ArrayCollection<V>(theValues);
+	}
 
-    private Object[] compact(final Object[] originalEntries) {
-        int originalLength = originalEntries.length;
-        ElementCardinality cardinality =
-                keyAndValueCardinality == DUPLICATE_KEYS_WITH_DUPLICATE_VALUES ? DUPLICATE_ELEMENTS : DISTINCT_ELEMENTS;
-        Entry<K, ModifiableCollection<V>>[] firstPass =
-                (Entry<K, ModifiableCollection<V>>[]) new Object[originalLength];
-        int j = -1;
-        for (int i = 0; i < originalLength; i++) {
-            if (i == 0 || !Objects.equals(((Entry<K, V>) originalEntries[i]).key(), firstPass[j].key())) {
-                j++;
-                firstPass[j] = new Entry<K, ModifiableCollection<V>>(((Entry<K, V>) originalEntries[i]).key(),
-                        new ModifiableArrayCollection<V>(cardinality));
-            }
-            firstPass[j].value().add(((Entry<K, V>) originalEntries[i]).value());
-        }
-        int resultLength = j + 1;
-        Entry<K, Collection<V>>[] result = (Entry<K, Collection<V>>[]) new Object[originalLength];
-        for (int i = 0; i < resultLength; i++) {
-            result[i] = new Entry<K, Collection<V>>(firstPass[i].key(), new ArrayCollection<V>(firstPass[i].value()));
-        }
-        return result;
-    }
+	private Object[] compact(final Object[] originalEntries) {
+		int originalLength = originalEntries.length;
+		ElementCardinality cardinality = keyAndValueCardinality == DUPLICATE_KEYS_WITH_DUPLICATE_VALUES
+				? DUPLICATE_ELEMENTS
+				: DISTINCT_ELEMENTS;
+		Object[] firstPass = new Object[originalLength];
+		int j = -1;
+		for (int i = 0; i < originalLength; i++) {
+			if (i == 0 || !Objects.equals(((Entry<K, V>) originalEntries[i]).key(),
+					((Entry<K, ModifiableCollection<V>>) firstPass[j]).key())) {
+				j++;
+				firstPass[j] = new Entry<K, ModifiableCollection<V>>(((Entry<K, V>) originalEntries[i]).key(),
+						new ModifiableArrayCollection<V>(cardinality));
+			}
+			((Entry<K, ModifiableCollection<V>>) firstPass[j]).value().add(((Entry<K, V>) originalEntries[i]).value());
+		}
+		int resultLength = j + 1;
+		Object[] result = new Object[originalLength];
+		for (int i = 0; i < resultLength; i++) {
+			result[i] = new Entry<K, Collection<V>>(((Entry<K, ModifiableCollection<V>>) firstPass[i]).key(),
+					new ArrayCollection<V>(((Entry<K, ModifiableCollection<V>>) firstPass[i]).value()));
+		}
+		return result;
+	}
 
-    @Override
-    public boolean contains(final Entry<K, V> element) {
-        Node<K, Collection<V>> node = sortedTree.getNode(element.key());
-        if (node == null) {
-            return false;
-        } else {
-            return node.getContent().contains(element.value());
-        }
-    }
+	@Override
+	public boolean contains(final Entry<K, V> element) {
+		Node<K, Collection<V>> node = sortedTree.getNode(element.key());
+		if (node == null) {
+			return false;
+		} else {
+			return node.getContent().contains(element.value());
+		}
+	}
 
-    @Override
-    public boolean containsAll(final Collection<?> collection) {
-        if (collection.size() > size()) {
-            return false;
-        }
-        return ArrayUtilities.containsAll(toArray(), size(), collection);
-    }
+	@Override
+	public boolean containsAll(final Collection<?> collection) {
+		if (collection.size() > size()) {
+			return false;
+		}
+		return ArrayUtilities.containsAll(toArray(), size(), collection);
+	}
 
-    @Override
-    public boolean containsKey(final K key) {
-        return keys.contains(key);
-    }
+	@Override
+	public boolean containsKey(final K key) {
+		return keys.contains(key);
+	}
 
-    @Override
-    public boolean containsValue(final V value) {
-        return values.contains(value);
-    }
+	@Override
+	public boolean containsValue(final V value) {
+		return values.contains(value);
+	}
 
-    @Override
-    public Entry<K, V> get() throws IndexOutOfBoundsException {
-        if (size == 0) {
-            throw new IndexOutOfBoundsException("Cannot return an entry from an empty map.");
-        } else {
-            Node<K, Collection<V>> root = sortedTree.getRootNode();
-            return new Entry<K, V>(root.getKey(), root.getContent().get());
-        }
-    }
+	@Override
+	public Entry<K, V> get() throws IndexOutOfBoundsException {
+		if (size == 0) {
+			throw new IndexOutOfBoundsException("Cannot return an entry from an empty map.");
+		} else {
+			Node<K, Collection<V>> root = sortedTree.getRootNode();
+			return new Entry<K, V>(root.getKey(), root.getContent().get());
+		}
+	}
 
-    @Override
-    public V get(final K key) throws IllegalArgumentException {
-        Node<K, Collection<V>> node = sortedTree.getNode(key);
-        if (node == null) {
-            throw new IllegalArgumentException("Map doesn't contain an entry with the key " + key + ".");
-        }
-        return node.getContent().get();
-    }
+	@Override
+	public V get(final K key) throws IllegalArgumentException {
+		Node<K, Collection<V>> node = sortedTree.getNode(key);
+		if (node == null) {
+			throw new IllegalArgumentException("Map doesn't contain an entry with the key " + key + ".");
+		}
+		return node.getContent().get();
+	}
 
-    @Override
-    public Collection<V> getAll(final K key) throws IllegalArgumentException {
-        Node<K, Collection<V>> node = sortedTree.getNode(key);
-        if (node == null) {
-            throw new IllegalArgumentException("Map doesn't contain an entry with the key " + key + ".");
-        }
-        return node.getContent();
-    }
+	@Override
+	public Collection<V> getAll(final K key) throws IllegalArgumentException {
+		Node<K, Collection<V>> node = sortedTree.getNode(key);
+		if (node == null) {
+			throw new IllegalArgumentException("Map doesn't contain an entry with the key " + key + ".");
+		}
+		return node.getContent();
+	}
 
-    @Override
-    public KeyAndValueCardinality getKeyAndValueCardinality() {
-        return keyAndValueCardinality;
-    }
+	@Override
+	public KeyAndValueCardinality getKeyAndValueCardinality() {
+		return keyAndValueCardinality;
+	}
 
-    @Override
-    public Collection<K> getKeys() {
-        return keys;
-    }
+	@Override
+	public Collection<K> getKeys() {
+		return keys;
+	}
 
-    @Override
-    public Collection<V> getValues() {
-        return values;
-    }
+	@Override
+	public Collection<V> getValues() {
+		return values;
+	}
 
-    @Override
-    public Iterator<Entry<K, V>> iterator() {
-        return new ArrayIterator<Entry<K, V>>(entries);
-    }
+	@Override
+	public Iterator<Entry<K, V>> iterator() {
+		return new ArrayIterator<Entry<K, V>>(entries);
+	}
 
-    @Override
-    public int size() {
-        return size;
-    }
+	@Override
+	public int size() {
+		return size;
+	}
 
-    @Override
-    public Spliterator<Entry<K, V>> spliterator() {
-        int characteristics = Spliterator.ORDERED | Spliterator.SORTED
-                | (keyAndValueCardinality == DISTINCT_KEYS ? Spliterator.DISTINCT : 0);
-        return new ArraySpliterator<Entry<K, V>>(entries, characteristics, entryByKeyComparator);
-    }
+	@Override
+	public Spliterator<Entry<K, V>> spliterator() {
+		int characteristics = Spliterator.ORDERED | Spliterator.SORTED
+				| (keyAndValueCardinality == DISTINCT_KEYS ? Spliterator.DISTINCT : 0);
+		return new ArraySpliterator<Entry<K, V>>(entries, characteristics, entryByKeyComparator);
+	}
 
-    @Override
-    public Object[] toArray() {
-        return entries.clone();
-    }
+	@Override
+	public Object[] toArray() {
+		return entries.clone();
+	}
 }
