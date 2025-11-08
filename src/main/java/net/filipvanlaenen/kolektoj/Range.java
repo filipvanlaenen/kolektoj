7d68fb5f9762e106ea8f751f71f68f48a471aa68
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-11-08 15:21:29 +0100

    Issue #89: Added unit tests on Range

--- src/main/java/net/filipvanlaenen/kolektoj/Range.java
+++ src/main/java/net/filipvanlaenen/kolektoj/Range.java
@@ -2,200 +2,384 @@ package net.filipvanlaenen.kolektoj;
 
 import java.util.Comparator;
 
+/**
+ * Interface defining what a range is.
+ *
+ * @param <E> The elemnt type.
+ */
 public interface Range<E> {
+    /**
+     * A range including all values.
+     *
+     * @param <E> The element type.
+     */
     public record All<E>() implements Range<E> {
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return false;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return false;
         }
     }
 
+    /**
+     * A range with only one value.
+     *
+     * @param value The value.
+     * @param <E>   The element type.
+     */
     public record EqualTo<E>(E value) implements Range<E> {
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, value) > 0;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, value) < 0;
         }
     }
 
+    /**
+     * A range with all values greater than a lower boundary.
+     *
+     * @param lower The lower boundary.
+     * @param <E>   The element type.
+     */
     public record GreaterThan<E>(E lower) implements Range<E> {
-        public GreaterThanAndLessThan<E> lessThan(E upper) {
+        /**
+         * Returns a new range restricting the current range to values less than an upper boundary.
+         *
+         * @param upper The upper boundary.
+         * @return A new range restricting the current range to values less than an upper boundary.
+         */
+        public GreaterThanAndLessThan<E> lessThan(final E upper) {
             return new GreaterThanAndLessThan<E>(lower, upper);
         }
 
-        public GreaterThanAndLessThanOrEqualTo<E> lessThanOrEqualTo(E upper) {
+        /**
+         * Returns a new range restricting the current range to values less than or equal to an upper boundary.
+         *
+         * @param upper The upper boundary.
+         * @return A new range restricting the current range to values less than or equal to an upper boundary.
+         */
+        public GreaterThanAndLessThanOrEqualTo<E> lessThanOrEqualTo(final E upper) {
             return new GreaterThanAndLessThanOrEqualTo<E>(lower, upper);
         }
 
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return false;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, lower) <= 0;
         }
     }
 
-    public record GreaterThanOrEqualTo<E>(E lower) implements Range<E> {
-        public GreaterThanOrEqualToAndLessThan<E> lessThan(E upper) {
-            return new GreaterThanOrEqualToAndLessThan<E>(lower, upper);
-        }
-
-        public GreaterThanOrEqualToAndLessThanOrEqualTo<E> lessThanOrEqualTo(E upper) {
-            return new GreaterThanOrEqualToAndLessThanOrEqualTo<E>(lower, upper);
-        }
-
-        @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
-            return false;
-        }
-
-        @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
-            return comparator.compare(element, lower) < 0;
-        }
-    }
-
-    public record LessThan<E>(E upper) implements Range<E> {
-        public GreaterThanAndLessThan<E> greaterThan(E lower) {
-            return new GreaterThanAndLessThan<E>(lower, upper);
-        }
-
-        public GreaterThanOrEqualToAndLessThan<E> greaterThanOrEqualTo(E lower) {
-            return new GreaterThanOrEqualToAndLessThan<E>(lower, upper);
-        }
-
-        @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
-            return comparator.compare(element, upper) >= 0;
-        }
-
-        @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
-            return false;
-        }
-    }
-
-    public record LessThanOrEqualTo<E>(E upper) implements Range<E> {
-        public GreaterThanAndLessThanOrEqualTo<E> greaterThan(E lower) {
-            return new GreaterThanAndLessThanOrEqualTo<E>(lower, upper);
-        }
-
-        public GreaterThanOrEqualToAndLessThanOrEqualTo<E> greaterThanOrEqualTo(E lower) {
-            return new GreaterThanOrEqualToAndLessThanOrEqualTo<E>(lower, upper);
-        }
-
-        @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
-            return comparator.compare(element, upper) > 0;
-        }
-
-        @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
-            return false;
-        }
-    }
-
+    /**
+     * A range with all values greater than a lower boundary and less than an upper boundary.
+     *
+     * @param lower The lower boundary.
+     * @param upper The upper boundary.
+     * @param <E>   The element type.
+     */
     public record GreaterThanAndLessThan<E>(E lower, E upper) implements Range<E> {
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, upper) >= 0;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, lower) <= 0;
         }
     }
 
+    /**
+     * A range with all values greater than a lower boundary and less than or equal to an upper boundary.
+     *
+     * @param lower The lower boundary.
+     * @param upper The upper boundary.
+     * @param <E>   The element type.
+     */
     public record GreaterThanAndLessThanOrEqualTo<E>(E lower, E upper) implements Range<E> {
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, upper) > 0;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, lower) <= 0;
         }
     }
 
+    /**
+     * A range with all values greater than or equal to a lower boundary.
+     *
+     * @param lower The lower boundary.
+     * @param <E>   The element type.
+     */
+    public record GreaterThanOrEqualTo<E>(E lower) implements Range<E> {
+        /**
+         * Returns a new range restricting the current range to values less than an upper boundary.
+         *
+         * @param upper The upper boundary.
+         * @return A new range restricting the current range to values less than an upper boundary.
+         */
+        public GreaterThanOrEqualToAndLessThan<E> lessThan(final E upper) {
+            return new GreaterThanOrEqualToAndLessThan<E>(lower, upper);
+        }
+
+        /**
+         * Returns a new range restricting the current range to values less than or equal to an upper boundary.
+         *
+         * @param upper The upper boundary.
+         * @return A new range restricting the current range to values less than or equal to an upper boundary.
+         */
+        public GreaterThanOrEqualToAndLessThanOrEqualTo<E> lessThanOrEqualTo(final E upper) {
+            return new GreaterThanOrEqualToAndLessThanOrEqualTo<E>(lower, upper);
+        }
+
+        @Override
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
+            return false;
+        }
+
+        @Override
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
+            return comparator.compare(element, lower) < 0;
+        }
+    }
+
+    /**
+     * A range with all values greater than or equal to a lower boundary and less than an upper boundary.
+     *
+     * @param lower The lower boundary.
+     * @param upper The upper boundary.
+     * @param <E>   The element type.
+     */
     public record GreaterThanOrEqualToAndLessThan<E>(E lower, E upper) implements Range<E> {
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, upper) >= 0;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, lower) < 0;
         }
     }
 
+    /**
+     * A range with all values greater than or equal to a lower boundary and less than or equal to an upper boundary.
+     *
+     * @param lower The lower boundary.
+     * @param upper The upper boundary.
+     * @param <E>   The element type.
+     */
     public record GreaterThanOrEqualToAndLessThanOrEqualTo<E>(E lower, E upper) implements Range<E> {
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, upper) > 0;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return comparator.compare(element, lower) < 0;
         }
     }
 
+    /**
+     * A range with all values less than an upper boundary.
+     *
+     * @param upper The upper boundary.
+     * @param <E>   The element type.
+     */
+    public record LessThan<E>(E upper) implements Range<E> {
+        /**
+         * Returns a new range restricting the current range to values greater than a lower boundary.
+         *
+         * @param lower The lower boundary.
+         * @return A new range restricting the current range to values greater than a lower boundary.
+         */
+        public GreaterThanAndLessThan<E> greaterThan(final E lower) {
+            return new GreaterThanAndLessThan<E>(lower, upper);
+        }
+
+        /**
+         * Returns a new range restricting the current range to values greater than or equal to a lower boundary.
+         *
+         * @param lower The lower boundary.
+         * @return A new range restricting the current range to values greater than or equal to a lower boundary.
+         */
+        public GreaterThanOrEqualToAndLessThan<E> greaterThanOrEqualTo(final E lower) {
+            return new GreaterThanOrEqualToAndLessThan<E>(lower, upper);
+        }
+
+        @Override
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
+            return comparator.compare(element, upper) >= 0;
+        }
+
+        @Override
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
+            return false;
+        }
+    }
+
+    /**
+     * A range with all values less than or equal to an upper boundary.
+     *
+     * @param upper The upper boundary.
+     * @param <E>   The element type.
+     */
+    public record LessThanOrEqualTo<E>(E upper) implements Range<E> {
+        /**
+         * Returns a new range restricting the current range to values greater than a lower boundary.
+         *
+         * @param lower The lower boundary.
+         * @return A new range restricting the current range to values greater than a lower boundary.
+         */
+        public GreaterThanAndLessThanOrEqualTo<E> greaterThan(final E lower) {
+            return new GreaterThanAndLessThanOrEqualTo<E>(lower, upper);
+        }
+
+        /**
+         * Returns a new range restricting the current range to values greater than or equal to a lower boundary.
+         *
+         * @param lower The lower boundary.
+         * @return A new range restricting the current range to values greater than or equal to a lower boundary.
+         */
+        public GreaterThanOrEqualToAndLessThanOrEqualTo<E> greaterThanOrEqualTo(final E lower) {
+            return new GreaterThanOrEqualToAndLessThanOrEqualTo<E>(lower, upper);
+        }
+
+        @Override
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
+            return comparator.compare(element, upper) > 0;
+        }
+
+        @Override
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
+            return false;
+        }
+    }
+
+    /**
+     * A range containing no values.
+     *
+     * @param <E> The element type.
+     */
     public record None<E>() implements Range<E> {
         @Override
-        public boolean isAbove(Comparator<? super E> comparator, E element) {
+        public boolean isAbove(final Comparator<? super E> comparator, final E element) {
             return true;
         }
 
         @Override
-        public boolean isBelow(Comparator<? super E> comparator, E element) {
+        public boolean isBelow(final Comparator<? super E> comparator, final E element) {
             return true;
         }
     }
 
+    /**
+     * Returns a range with all values.
+     *
+     * @param <F> The element type.
+     * @return A range with all values.
+     */
     static <F> All<F> all() {
         return new All<F>();
     }
 
-    static <F> EqualTo<F> equalTo(F value) {
+    /**
+     * Returns a range with a single value.
+     *
+     * @param <F>   The element type.
+     * @param value The value.
+     * @return A range with a single value.
+     */
+    static <F> EqualTo<F> equalTo(final F value) {
         return new EqualTo<F>(value);
     }
 
-    static <F> GreaterThan<F> greaterThan(F lower) {
+    /**
+     * Returns a range with all values greater than a lower boundary.
+     *
+     * @param <F>   The element type.
+     * @param lower The lower boundary.
+     * @return A range with all values greater than a lower boundary.
+     */
+    static <F> GreaterThan<F> greaterThan(final F lower) {
         return new GreaterThan<F>(lower);
     }
 
-    static <F> GreaterThanOrEqualTo<F> greaterThanOrEqualTo(F lower) {
+    /**
+     * Returns a range with all values greater than or equal to a lower boundary.
+     *
+     * @param <F>   The element type.
+     * @param lower The lower boundary.
+     * @return A range with all values greater than or equal to a lower boundary.
+     */
+    static <F> GreaterThanOrEqualTo<F> greaterThanOrEqualTo(final F lower) {
         return new GreaterThanOrEqualTo<F>(lower);
     }
 
-    static <F> LessThan<F> lessThan(F upper) {
+    /**
+     * Returns a range with all values less than an upper boundary.
+     *
+     * @param <F>   The element type.
+     * @param upper The upper boundary.
+     * @return A range with all values less than an upper boundary.
+     */
+    static <F> LessThan<F> lessThan(final F upper) {
         return new LessThan<F>(upper);
     }
 
-    static <F> LessThanOrEqualTo<F> lessThanOrEqualTo(F upper) {
+    /**
+     * Returns a range with all values less than an or equal to upper boundary.
+     *
+     * @param <F>   The element type.
+     * @param upper The upper boundary.
+     * @return A range with all values less than an or equal to upper boundary.
+     */
+    static <F> LessThanOrEqualTo<F> lessThanOrEqualTo(final F upper) {
         return new LessThanOrEqualTo<F>(upper);
     }
 
+    /**
+     * Returns a range with no values.
+     *
+     * @param <F> The element type.
+     * @return A range with no values.
+     */
     static <F> None<F> none() {
         return new None<F>();
     }
 
+    /**
+     * Returns true if the provided element is above the range according to the provided comparator.
+     *
+     * @param comparator The comparator.
+     * @param element    The element.
+     * @return True if the provided element is above the range according to the provided comparator, and false
+     *         otherwise.
+     */
     boolean isAbove(Comparator<? super E> comparator, E element);
 
+    /**
+     * Returns true if the provided element is below the range according to the provided comparator.
+     *
+     * @param comparator The comparator.
+     * @param element    The element.
+     * @return True if the provided element is below the range according to the provided comparator, and false
+     *         otherwise.
+     */
     boolean isBelow(Comparator<? super E> comparator, E element);
 }
--- src/test/java/net/filipvanlaenen/kolektoj/RangeTest.java
+++ src/test/java/net/filipvanlaenen/kolektoj/RangeTest.java
@@ -1,6 +1,8 @@
 package net.filipvanlaenen.kolektoj;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Comparator;
 
@@ -13,7 +15,104 @@ public class RangeTest {
     /**
      * The magic number three.
      */
+    private static final int THREE = 3;
+    /**
+     * The magic number four.
+     */
+    private static final int FOUR = 4;
+    /**
+     * The range with all values.
+     */
     private static final Range<Integer> ALL = Range.all();
+    /**
+     * The range with no values.
+     */
+    private static final Range<Integer> NONE = Range.none();
+    /**
+     * The range with the single value one.
+     */
+    private static final Range<Integer> ONE = Range.equalTo(1);
+    /**
+     * The range greater than one.
+     */
+    private static final Range<Integer> GREATER_THAN_ONE = Range.greaterThan(1);
+    /**
+     * The range greater than or equal to one.
+     */
+    private static final Range<Integer> GREATER_THAN_OR_EQUAL_TO_ONE = Range.greaterThanOrEqualTo(1);
+    /**
+     * The range greater than one and less than three.
+     */
+    private static final Range<Integer> GREATER_THAN_ONE_AND_LESS_THAN_THREE = Range.greaterThan(1).lessThan(THREE);
+    /**
+     * The range greater than one and less than or equal to three.
+     */
+    private static final Range<Integer> GREATER_THAN_ONE_AND_LESS_THAN_OR_EQUAL_TO_THREE =
+            Range.greaterThan(1).lessThanOrEqualTo(THREE);
+    /**
+     * The range less than one.
+     */
+    private static final Range<Integer> LESS_THAN_ONE = Range.lessThan(1);
+    /**
+     * The range less than or equal to one.
+     */
+    private static final Range<Integer> LESS_THAN_ONE_OR_EQUAL_TO = Range.lessThanOrEqualTo(1);
+
+    /**
+     * Verifies that the factory method creates a range for all values.
+     */
+    @Test
+    public void allShouldCreateARangeForAllValues() {
+        assertEquals(new Range.All<Integer>(), Range.all());
+    }
+
+    /**
+     * Verifies that the factory method creates a range for no values.
+     */
+    @Test
+    public void noneShouldCreateARangeForNoValues() {
+        assertEquals(new Range.None<Integer>(), Range.none());
+    }
+
+    /**
+     * Verifies that the factory method creates a range for one value.
+     */
+    @Test
+    public void equalToShouldCreateRangeForOneValue() {
+        assertEquals(new Range.EqualTo<Integer>(1), Range.equalTo(1));
+    }
+
+    /**
+     * Verifies that the factory method creates a range for greater than a value.
+     */
+    @Test
+    public void greaterThanShouldCreateRangeForGreaterThanAValue() {
+        assertEquals(new Range.GreaterThan<Integer>(1), Range.greaterThan(1));
+    }
+
+    /**
+     * Verifies that the factory method creates a range for greater than or equal to a value.
+     */
+    @Test
+    public void greaterThanOrEqualToShouldCreateRangeForGreaterThanOrEqualToAValue() {
+        assertEquals(new Range.GreaterThanOrEqualTo<Integer>(1), Range.greaterThanOrEqualTo(1));
+    }
+
+    /**
+     * Verifies that the factory method creates a range for less than a value.
+     */
+    @Test
+    public void lessThanShouldCreateRangeForLessThanAValue() {
+        assertEquals(new Range.LessThan<Integer>(1), Range.lessThan(1));
+    }
+
+    /**
+     * Verifies that the factory method creates a range for less than or equal to a value.
+     */
+    @Test
+    public void lessThanOrEqualToShouldCreateRangeForLessThanOrEqualToAValue() {
+        assertEquals(new Range.LessThanOrEqualTo<Integer>(1), Range.lessThanOrEqualTo(1));
+    }
 
     /**
      * Verifies that zero is not above all.
@@ -29,5 +128,213 @@ public class RangeTest {
     @Test
     public void zeroShouldNotBeBelowAll() {
         assertFalse(ALL.isBelow(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that zero is above none.
+     */
+    @Test
+    public void zeroShouldBeAboveNone() {
+        assertTrue(NONE.isAbove(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that zero is below none.
+     */
+    @Test
+    public void zeroShouldBeBelowNone() {
+        assertTrue(NONE.isBelow(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that zero is below one.
+     */
+    @Test
+    public void zeroShouldBeBelowOne() {
+        assertTrue(ONE.isBelow(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that one isn't below one.
+     */
+    @Test
+    public void oneShouldNotBeBelowOne() {
+        assertFalse(ONE.isBelow(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that one isn't above one.
+     */
+    @Test
+    public void oneShouldNotBeAboveOne() {
+        assertFalse(ONE.isAbove(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that two is above one.
+     */
+    @Test
+    public void twoShouldBeAboveOne() {
+        assertTrue(ONE.isAbove(Comparator.naturalOrder(), 2));
+    }
+
+    /**
+     * Verifies that one is below greater than one.
+     */
+    @Test
+    public void oneShouldBeBelowGreaterThanOne() {
+        assertTrue(GREATER_THAN_ONE.isBelow(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that two isn't below greater than one.
+     */
+    @Test
+    public void twoShouldNotBeBelowGreaterThanOne() {
+        assertFalse(GREATER_THAN_ONE.isBelow(Comparator.naturalOrder(), 2));
+    }
+
+    /**
+     * Verifies that two isn't above greater than one.
+     */
+    @Test
+    public void twoShouldNotBeAboveGreaterThanOne() {
+        assertFalse(GREATER_THAN_ONE.isAbove(Comparator.naturalOrder(), 2));
+    }
+
+    /**
+     * Verifies that zero is below greater than or equal to one.
+     */
+    @Test
+    public void zeroShouldBeBelowGreaterThanOrEqualToOne() {
+        assertTrue(GREATER_THAN_OR_EQUAL_TO_ONE.isBelow(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that one isn't below greater than or equal to one.
+     */
+    @Test
+    public void oneShouldNotBeBelowGreaterThanOrEqualToOne() {
+        assertFalse(GREATER_THAN_OR_EQUAL_TO_ONE.isBelow(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that two isn't above greater than or equal to one.
+     */
+    @Test
+    public void twoShouldNotBeAboveGreaterThanOrEqualToOne() {
+        assertFalse(GREATER_THAN_OR_EQUAL_TO_ONE.isAbove(Comparator.naturalOrder(), 2));
+    }
+
+    /**
+     * Verifies that one is below greater than one and less than three.
+     */
+    @Test
+    public void oneShouldBeBelowGreaterThanOneAndLessThanThree() {
+        assertTrue(GREATER_THAN_ONE_AND_LESS_THAN_THREE.isBelow(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that two isn't below greater than one and less than three.
+     */
+    @Test
+    public void twoShouldNotBeBelowGreaterThanOneAndLessThanThree() {
+        assertFalse(GREATER_THAN_ONE_AND_LESS_THAN_THREE.isBelow(Comparator.naturalOrder(), 2));
+    }
+
+    /**
+     * Verifies that two isn't above greater than one and less than three.
+     */
+    @Test
+    public void twoShouldNotBeAboveGreaterThanOneAndLessThanThree() {
+        assertFalse(GREATER_THAN_ONE_AND_LESS_THAN_THREE.isAbove(Comparator.naturalOrder(), 2));
+    }
+
+    /**
+     * Verifies that three is above greater than one and less than three.
+     */
+    @Test
+    public void threeShouldBeAboveGreaterThanOneAndLessThanThree() {
+        assertTrue(GREATER_THAN_ONE_AND_LESS_THAN_THREE.isAbove(Comparator.naturalOrder(), THREE));
+    }
+
+    /**
+     * Verifies that one is below greater than one and less than or equal to three.
+     */
+    @Test
+    public void oneShouldBeBelowGreaterThanOneAndLessThanOrEqualToThree() {
+        assertTrue(GREATER_THAN_ONE_AND_LESS_THAN_OR_EQUAL_TO_THREE.isBelow(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that two isn't below greater than one and less than or equal to three.
+     */
+    @Test
+    public void twoShouldNotBeBelowGreaterThanOneAndLessThanOrEqualToThree() {
+        assertFalse(GREATER_THAN_ONE_AND_LESS_THAN_OR_EQUAL_TO_THREE.isBelow(Comparator.naturalOrder(), 2));
+    }
+
+    /**
+     * Verifies that three isn't above greater than one and less than or equal to three.
+     */
+    @Test
+    public void threeShouldNotBeAboveGreaterThanOneAndLessThanOrEqualToThree() {
+        assertFalse(GREATER_THAN_ONE_AND_LESS_THAN_OR_EQUAL_TO_THREE.isAbove(Comparator.naturalOrder(), THREE));
+    }
+
+    /**
+     * Verifies that four is above greater than one and less than or equal to three.
+     */
+    @Test
+    public void fourShouldBeAboveGreaterThanOneAndLessThanOrEqualToThree() {
+        assertTrue(GREATER_THAN_ONE_AND_LESS_THAN_OR_EQUAL_TO_THREE.isAbove(Comparator.naturalOrder(), FOUR));
+    }
+
+    /**
+     * Verifies that zero isn't below less than one.
+     */
+    @Test
+    public void zeroShouldNotBeBelowLessThanOne() {
+        assertFalse(LESS_THAN_ONE.isBelow(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that zero isn't above less than one.
+     */
+    @Test
+    public void zeroShouldNotBeAboveLessThanOne() {
+        assertFalse(LESS_THAN_ONE.isAbove(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that one is above less than one.
+     */
+    @Test
+    public void oneShouldBeAboveLessThanOne() {
+        assertTrue(LESS_THAN_ONE.isAbove(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that zero isn't below less than or equal to one.
+     */
+    @Test
+    public void zeroShouldNotBeBelowLessThanOrEqualToOne() {
+        assertFalse(LESS_THAN_ONE_OR_EQUAL_TO.isBelow(Comparator.naturalOrder(), 0));
+    }
+
+    /**
+     * Verifies that one isn't above less than or equal to one.
+     */
+    @Test
+    public void oneShouldNotBeAboveLessThanOrEqualToOne() {
+        assertFalse(LESS_THAN_ONE_OR_EQUAL_TO.isAbove(Comparator.naturalOrder(), 1));
+    }
+
+    /**
+     * Verifies that two is above less than or equal to one.
+     */
+    @Test
+    public void twoShouldBeAboveLessThanOrEqualToOne() {
+        assertTrue(LESS_THAN_ONE_OR_EQUAL_TO.isAbove(Comparator.naturalOrder(), 2));
     }
 }
