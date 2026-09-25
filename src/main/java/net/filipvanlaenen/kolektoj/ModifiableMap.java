64d7034f85d1494294eb7804d5e5f1b5b080fde1
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-09-25 15:36:52 +0200

    Issue #113 and #118: Added intersectionOf and differenceOf to ModifiableMap

--- src/main/java/net/filipvanlaenen/kolektoj/ModifiableMap.java
+++ src/main/java/net/filipvanlaenen/kolektoj/ModifiableMap.java
@@ -13,6 +13,30 @@ import net.filipvanlaenen.kolektoj.hash.ModifiableHashMap;
  */
 public interface ModifiableMap<K, V> extends Collection<Entry<K, V>>, UpdatableMap<K, V> {
     /**
+     * Returns a new modifiable map containing all the entries present in the first map, but not in any of the other
+     * provided maps.
+     *
+     * This method corresponds to the difference (or relative complement) operation in set theory, denoted by the symbol
+     * ∖, with {1, 2, 3} ∖ {2, 3, 4} = {1}.
+     *
+     * @param <L>  The key type.
+     * @param <W>  The value type.
+     * @param maps The maps from which to calculate the difference.
+     * @return A new modifiable map containing all the entries present in the first map, but not in any of the other
+     *         provided maps.
+     */
+    static <L, W> Map<L, W> differenceOf(final Map<? extends L, ? extends W>... maps) {
+        if (maps.length == 0) {
+            return empty();
+        }
+        ModifiableMap<L, W> result = ModifiableMap.of(maps[0]);
+        for (int i = 1; i < maps.length; i++) {
+            result.removeAll(maps[i]);
+        }
+        return result;
+    }
+
+    /**
      * Returns a new empty modifiable map.
      *
      * @param <L> The key type.
@@ -21,6 +45,28 @@ public interface ModifiableMap<K, V> extends Collection<Entry<K, V>>, UpdatableM
      */
     static <L, W> ModifiableMap<L, W> empty() {
         return new ModifiableHashMap<L, W>();
+    }
+
+    /**
+     * Returns a new modifiable map containing all the entries present in each of the provided maps.
+     *
+     * This method corresponds to the intersection operation in set theory, denoted by the symbol ∩, with {1, 2, 3} ∩
+     * {2, 3, 4} = {2, 3}.
+     *
+     * @param <L>  The key type.
+     * @param <W>  The value type.
+     * @param maps The maps from which to calculate the intersection.
+     * @return A new modifiable map containing all the entries present in each of the provided maps.
+     */
+    static <L, W> Map<L, W> intersectionOf(final Map<? extends L, ? extends W>... maps) {
+        if (maps.length == 0) {
+            return empty();
+        }
+        ModifiableMap<L, W> result = ModifiableMap.of(maps[0]);
+        for (int i = 1; i < maps.length; i++) {
+            result.retainAll(maps[i]);
+        }
+        return result;
     }
 
     /**
